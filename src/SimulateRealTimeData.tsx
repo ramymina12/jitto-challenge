99b62c342ac2c4481e92a7970a202e5656d80577
Ramy <ramy.mina@mail.utoronto.ca>
2025-07-22 21:46:14 -0400

    Declared types and did vite built.

--- src/SimulateRealTimeData.tsx
+++ src/SimulateRealTimeData.tsx
@@ -1,19 +1,23 @@
-export const SimulateRealTimeData = (sensorCount, interval, onUpdate) => {
-  const generateMetrics = (sensorId) => ({
-    sensorId: sensorId,
-    timestamp: new Date().toISOString(),
-    airQuality: +(Math.random() * 200).toFixed(2), // 0-200 AQI (Good to Moderate)
-    temperature: +(10 + Math.random() * 30).toFixed(2), // 10-40°C
-    humidity: +(30 + Math.random() * 60).toFixed(2), // 30-90% RH
-  });
+export const SimulateRealTimeData = (
+    sensorCount: number,
+    interval: number,
+    onUpdate: Function
+) => {
+    const generateMetrics = (sensorId: string) => ({
+        sensorId: sensorId,
+        timestamp: new Date().toISOString(),
+        airQuality: +(Math.random() * 200).toFixed(2), // 0-200 AQI (Good to Moderate)
+        temperature: +(10 + Math.random() * 30).toFixed(2), // 10-40°C
+        humidity: +(30 + Math.random() * 60).toFixed(2), // 30-90% RH
+    });
 
-  const intervalId = setInterval(() => {
-    const updates = Array.from({ length: sensorCount }, (_, i) =>
-      generateMetrics(`Sensor-${i + 1}`)
-    );
-    onUpdate(updates);
-  }, interval);
+    const intervalId = setInterval(() => {
+        const updates = Array.from({ length: sensorCount }, (_, i) =>
+            generateMetrics(`Sensor-${i + 1}`)
+        );
+        onUpdate(updates);
+    }, interval);
 
-  return () => clearInterval(intervalId);
+    return () => clearInterval(intervalId);
 };
 export default SimulateRealTimeData;
